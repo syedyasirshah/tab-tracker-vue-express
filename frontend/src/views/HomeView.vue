83a945df377dd14a4a5e39d389042e0e427239ca
syedyasirshah <yasirshab@gmail.com>
2023-05-18 18:41:59 +0500

    Set up frontend

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="home">
+		Home Component
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "HomeView",
+	components: {},
+});
+</script>
